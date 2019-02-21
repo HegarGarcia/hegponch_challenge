c3a88d65e298061a03cc97a8e0f9cc1458da1a47
Hegar Garcia <hegargarcia@gmai.com>
2019-02-21 12:42:57 -0600

    Adding Moves

---
+++ rubik/typings/index.d.ts
@@ -0,0 +1,22 @@
+declare namespace RubikCube {
+  enum IMove {
+    "B",
+    "B'",
+    "B2",
+    "D",
+    "D'",
+    "D2",
+    "F",
+    "F'",
+    "F2",
+    "L",
+    "L'",
+    "L2",
+    "R",
+    "R'",
+    "R2",
+    "U",
+    "U'",
+    "U2",
+  }
+}
